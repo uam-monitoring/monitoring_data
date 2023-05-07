16ffbe3bc4110f48f89f40e086fd8ab685404ffd
kth1210 <k2hop1210@gmail.com>
2023-05-07 23:36:51 +0900

    [FEAT] : ADS-B 데이터 생성, 속도 72km 가정

--- UAM_ADS-B_JSON.py
+++ UAM_ADS-B_JSON.py
@@ -1,4 +1,5 @@
 import json
+from haversine import haversine, Unit
 
 def make_UAM_ADSB_JSON(idx, uamIdentification, date, time, timeReference, altitude, longitude, latitude):
     
@@ -22,16 +23,39 @@ def make_UAM_ADSB_JSON(idx, uamIdentification, date, time, timeReference, altitu
         json.dump(UAM_ADSB, f, indent=4)
 
 
-if __name__ == "__main__":
+def get_waypoints(departure, arrival):
+    waypoints = []
+    distance = haversine(departure, arrival, unit = "m")
 
-    make_UAM_ADSB_JSON(
-        idx = "1",
-        uamIdentification = "UAL123",
-        date = "2025-05-25",
-        time = "14:12:00",
-        timeReference = "UTC",
-        altitude = "400",
-        longitude = "126.9525465",
-        latitude = "37.5453577"
-    )
+    div = distance // 100
+
+    lat_unit = (departure[0] - arrival[0]) / div
+    lon_unit = (departure[1] - arrival[1]) / div
+
+    waypoints.append(departure)
+    for mul in range(1, int(div)):
+        waypoints.append((departure[0] - lat_unit * mul, departure[1] - lon_unit * mul))
+    waypoints.append(arrival)
+    
+    return waypoints
+
+
+if __name__ == "__main__":
+    
+    departure = (35.887983, 128.606638)
+    arrival = (35.879337, 128.627656)
+
+    waypoints = get_waypoints(departure, arrival)
+
+    for idx, point in enumerate(waypoints):
+        make_UAM_ADSB_JSON(
+            idx = str(idx),
+            uamIdentification = "BKDG001",
+            date = "2025-05-25",
+            time = "14:{min}:{sec}".format(min = idx // 12, sec = idx * 5 % 60),
+            timeReference = "UTC",
+            altitude = 300,
+            longitude = point[1],
+            latitude = point[0]
+        )   
 
